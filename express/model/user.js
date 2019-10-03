a1b9aafb8dc0f28b697a2e4dab1baa6ebb058afe
zNext666 <alonereview@gmail.com>
2019-10-03 15:24:52 +0700

    add migrations for database/orm

--- express/model/user.js
+++
@@ -1,8 +0,0 @@
-module.exports = (sequelize, DataTypes) => {
-    class User extends sequelize.Model { }
-    User.init({
-      username: DataTypes.STRING(100),
-      password: DataTypes.STRING(1024)
-    }, { sequelize,timestamps: true });
-    return User;
-}
--- express/server.js
+++ express/server.js
@@ -1,127 +1,20 @@
 const express = require('express')
-const Sequelize = require('sequelize');
-const Model = Sequelize.Model;
+const listEndpoints = require('express-list-endpoints')
 const app = express()
-
 const port = 8000;
-const hostname = '127.0.0.1';
-const database = 'cmuclassrater';
-const username = 'root';
-const password = 'alonealoha5';
-
-// connect database
-const sequelize = new Sequelize(database, username, password, {
-  host: hostname,
-  dialect: 'mariadb',
-  dialectOptions: {connectTimeout: 1000},
-  define: {
-    // The `timestamps` field specify whether or not the `createdAt` and `updatedAt` fields will be created.
-    // This was true by default, but now is false by default
-    timestamps: true
-  }
-
-});
-
-// test connection
-sequelize
-  .authenticate()
-  .then(() => {
-    console.log('Connection has been established successfully.');
-  })
-  .catch(err => {
-    console.error('Unable to connect to the database:', err);
-  });
-
-
-// Model
-class User extends Model {}
-User.init({
-  username: {
-    type: Sequelize.DataTypes.STRING(100),
-    allowNull: false
-  },
-  password: {
-      type: Sequelize.DataTypes.STRING(1024),
-      allowNull: false
-  }
-}, {
-  sequelize,
-  modelName: 'user',
-  timestamps: true
-});
-
-class Course extends Model {}
-Course.init({
-    course_no: {
-        type: Sequelize.DataTypes.STRING(40),
-        allowNull: false
-    },
-    name: {
-        type: Sequelize.DataTypes.STRING(200),
-        allowNull: false
-    }
-},{
-    sequelize,
-    modelName: 'course',
-    timestamps: true
-});
-
-class Section extends Model {}
-Section.init({
-    sec_no :{
-        type: Sequelize.DataTypes.STRING(10),
-        allowNull: false
-    }
-},{
-    sequelize,
-    modelName: 'section',
-    timestamps: true
-});
-
-class Teacher extends Model {}
-Teacher.init({
-    name: {
-        type: Sequelize.DataTypes.STRING(100),
-        allowNull: false
-    }
-},{
-    sequelize,
-    modelName: 'teacher',
-    timestamps: true
-});
-class Review extends Model {}
-Review.init({
-    rate: {
-        type: Sequelize.DataTypes.FLOAT,
-        allowNull: false
-    },
-    commend: {
-        type: Sequelize.DataTypes.TEXT,
-        allowNull: false
-    }
-},{
-    sequelize,
-    modelName: 'review',
-    timestamps: true
-});
-
-// Associations
-
-User.hasMany(Review);
-Review.belongsTo(User);
-
-Course.hasMany(Review);
-Review.belongsTo(Course);
-
-Course.hasMany(Section);
-Section.belongsTo(Course);
-
-Section.hasMany(Teacher);
-Teacher.hasMany(Section);
-
-//const User = sequelize.import("model/user");
-
-// Sync
 
 // run express
+app.use(express.json()) // for parsing application/json
+app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 app.listen(port, () => console.log(`CMU-Class-Rater API listening on port ${port}!`))
+
+// Maping Routes
+app.use(require('./routes/index'))
+console.log(listEndpoints(app));
+
+// log to console
+app.use('/',function(req, res, next){
+    console.log("A new request received at " + Date.now())
+    next()
+ });
+ 
