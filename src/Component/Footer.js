c4f2358c5130554b5d34c4e631fdfe4c65adda83
zNext666 <alonereview@gmail.com>
2019-10-07 15:03:49 +0700

    fixed position footer by using navbar

--- src/Component/Footer.js
+++ src/Component/Footer.js
@@ -1,10 +1,12 @@
 import React from 'react'
-
+import {Navbar} from 'react-bootstrap'
 function Footer(){
     return (
-        <footer>
-            CMU Class Rater is licensed under the GNU General Public License v3.0
-        </footer>
+        <Navbar fixed="bottom">
+            <footer>
+                CMU Class Rater is licensed under the GNU General Public License v3.0
+            </footer>
+        </Navbar>
     )
 }
 
