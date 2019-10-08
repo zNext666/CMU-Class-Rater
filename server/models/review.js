10844057d52745a822d0ef516cafffd01e822d51
Purit <tunesudro@hotmail.com>
2019-10-08 15:30:03 +0700

    add relation btw review & course

--- server/models/review.js
+++ server/models/review.js
@@ -3,14 +3,12 @@ module.exports = (sequelize, DataTypes) => {
   const Review = sequelize.define('Review', {
     user: DataTypes.STRING,
     rate: DataTypes.DOUBLE,
-    comment: DataTypes.STRING
+    comment: DataTypes.STRING,
+    course_no: DataTypes.INTEGER
   }, {});
   Review.associate = function(models) {
     // associations can be defined here
-    Review.belongsTo(models.Course,{
-      foreignKey: 'course_id',
-      as: 'course_id'
-    })
+    Review.belongsTo(models.Course);
   };
   return Review;
 };
