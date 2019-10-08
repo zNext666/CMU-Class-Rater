e7a1b99b250a73f58cc368bb7dcae2e2da6cbfbf
Purit <tunesudro@hotmail.com>
2019-10-08 10:49:15 +0700

    change layout

--- src/Component/Header.js
+++ src/Component/Header.js
@@ -1,20 +1,29 @@
 import React, { Component } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import {Nav,Form,Navbar} from 'react-bootstrap';
+import { Nav, Form, Navbar, Container } from 'react-bootstrap';
 
 class Header extends Component{
     render(){
+        const style={
+            position:'absolute',
+            right:'50px'
+        }
+        const auth = 'Anonymous'
+        if(sessionStorage.getItem('auth')){
+            auth = sessionStorage.getItem('auth')
+        }
         return(
             <header>
-                <Navbar bg="light" expand="lg">
+                <Navbar bg="light" expand="lg" >
                     <Navbar.Brand href="/">CMU Class Rater</Navbar.Brand>
                     <Nav.Link href="/">Home</Nav.Link>
+                    <p style={style}>{auth}</p>
                 </Navbar>
                 <Nav>
                     <Nav.Item as="li">
-                        <Form.Control type="text" placeholder="Search" style={{position:"relative"}}/>
+                        <Form.Control type="text" placeholder="Search" />
                     </Nav.Item>
-                </Nav>
+                </Nav>             
             </header>
         )
     }
--- src/Component/Review.js
+++ src/Component/Review.js
@@ -24,8 +24,21 @@ class Review extends Component{
         this.onSubmit = this.onSubmit.bind(this)
     }
 
-    onSubmit(){
-        alert('rate: '+  this.state.rate + ' comment: ' + this.state.review)
+    async onSubmit(){
+        const auth = 'Anonymous'
+        if(sessionStorage.getItem('auth')){
+            auth = sessionStorage.getItem('auth')
+        }
+
+        const ncomment = await {
+            user: auth,
+            rating: this.state.rate,
+            comment: this.state.review
+        }
+        //console.log(ncomment)
+        this.state.comment.push(ncomment)
+        console.log(this.state.comment)
+        //alert('rate: '+  this.state.rate + ' comment: ' + this.state.review)
     }
 
     handleChange(e){
@@ -34,7 +47,7 @@ class Review extends Component{
 
     render(){
         const comment = this.state.comment.map(comm => (
-            <Card.Body>
+            <Card.Body key={comm.user}>
                 <h5>{comm.user}</h5>
                 <Rating initialRating={comm.rating} readonly />
                 <p>{comm.comment}</p>
@@ -50,8 +63,8 @@ class Review extends Component{
                 <Tab eventKey="Review" title="Review">
                     <Form>
                         <Rating onChange={(rate) => this.handleChange(rate)} fractions={2} />
-                        <Form.Control onChange={event => this.setState({review: event.target.value})} as="textarea" rows="3" placeholder="Write a review..."/>
-                        <Button variant="primary" onClick={this.onSubmit} >Review</Button>
+                        <Form.Control onChange={event => this.state.review = event.target.value} as="textarea" rows="3" placeholder="Write a review..."/>
+                        <Button type="submit" variant="primary" onClick={this.onSubmit} >Review</Button>
                     </Form>
                 </Tab>
             </Tabs>
