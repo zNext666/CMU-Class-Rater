b8cb1789f873b000a648e978af740d6fb7d1aa2c
zNext666 <alonereview@gmail.com>
2019-10-25 11:00:44 +0700

    Add search for course

--- backend/routes/course/course.js
+++ backend/routes/course/course.js
@@ -2,6 +2,41 @@ const express = require('express')
 const router = express.Router()
 const db = require('../../models/index')
 const Course = db.Course
+const Op = db.Sequelize.Op
+router.get('/search',(req,res) => {
+    var search = req.query.search
+    console.log(Number(search))
+    if(isNaN(Number(search))){
+        Course.findAll({
+            attributes : ['course_no','name'],
+            where: {
+                name: {
+                    [Op.like]: search +'%'
+                }
+            }
+        })
+        .then((result)=>{
+            //console.log(result)
+            res.json(result)
+        })
+    }else{
+        Course.findAll({
+            attributes : ['course_no','name'],
+            where: {
+                course_no: {
+                    [Op.like]: search +'%'
+                }
+                }
+        })
+        .then((result)=>{
+            //console.log(result)
+            res.json(result)
+        })
+
+    }
+
+})
+
 router.get('/:course_no',(req,res) => {
     Course.findOne({
       attributes : ['course_no','name','section','teacher','description'],
