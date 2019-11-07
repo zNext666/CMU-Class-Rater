d57926856faeed2e93736dafda3d9e690c3b2b36
zNext666 <alonereview@gmail.com>
2019-11-08 00:08:21 +0700

    fix hash password

--- backend/routes/users/users.js
+++ backend/routes/users/users.js
@@ -5,20 +5,13 @@ const db = require('../../models/index')
 const { registerValidationRules, validate } = require('./../../validator')
 const User = db.User
 const Op = db.Sequelize.Op
-const saltRounds = 10;
+const saltRounds = 10
 
 router.post('/register',registerValidationRules(),validate,(req,res) => {
-
-    // hash a password
-    var hashPWD
-    bcrypt.hash(req.body.password, saltRounds, (err, hash) => {
-        hashPWD = hash
-    })
- 
     User.create({
         username:req.body.username,
         email:req.body.email,
-        password:hashPWD,
+        password:bcrypt.hashSync(req.body.password,saltRounds),
         role:{role:['user']}
     }).then(data=>{
         res.json(data)
