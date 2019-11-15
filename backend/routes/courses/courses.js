3a927cc0b25a4c09d3b2b2af5f53505f31e30ec6
zNext666 <alonereview@gmail.com>
2019-11-15 12:17:25 +0700

    add average return on courses list

--- backend/routes/courses/courses.js
+++ backend/routes/courses/courses.js
@@ -2,12 +2,23 @@ const express = require('express')
 const router = express.Router()
 const db = require('../../models/index')
 const Course = db.Course
+const Review = db.Review
 
 router.get('',(req,res) => {
     Course.findAll({
-      attributes : ['course_no','name','section','teacher','description']
+      include: [
+        { 
+          required: true,
+          model: Review,
+          attributes: ['course_no',[db.sequelize.fn('avg', db.sequelize.col('rate')), 'average']]
+        }
+     ],
+      attributes : ['course_no','name','section','teacher','description'],
+      group: ['course_no']
     }).then((data) => {
       res.json(data)
+    }).catch((error) => {
+      res.status(500)
     })
   })
   
